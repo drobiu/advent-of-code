c67630a51e92c837a66dc6ef3010a102913b0459
drobiu <32435586+drobiu@users.noreply.github.com>
2019-12-15 03:13:05 +0100

    Add day 5

---
+++ javascript/scripts/day_5_1.js
@@ -0,0 +1,45 @@
+let fs = require('fs');
+let input = fs.readFileSync('data_5.txt', 'utf8').split(',');
+for (var j = 0; j < input.length; j++) 
+    input[j] = parseInt(input[j]);
+
+let i = 0;
+
+while (input[i] != 99 && i < input.length) {
+    let instruction = input[i];
+
+    let opcode = instruction % 100;
+    instruction = Math.floor(instruction /= 100);
+
+    let params = [];
+    let point = 0;   
+    while (point < 3) {
+        params[point] = instruction % 10;
+        instruction = Math.floor(instruction /= 10);
+        point++;
+    }
+
+    let param1 = params[0] == 0 ? input[input[i+1]] : input[i+1];
+    let param2 = params[1] == 0 ? input[input[i+2]] : input[i+2];
+    let param3 = params[2] == 0 ? input[i+3] : i+3;
+
+    switch(opcode) {
+        case 1:
+            input[param3] = param1 + param2;
+            i += 4;
+            break;
+        case 2:
+            input[param3] = param1 * param2;
+            i += 4;
+            break;
+        case 3:
+            let param4 = params[0] == 0 ? input[i+1] : i+1;
+            input[param4] = 1;
+            i += 2;
+            break;
+        case 4:
+            console.log(params[0] == 0 ? input[input[i+1]] : input[i+1]);
+            i += 2;
+            break;
+    } 
+}
